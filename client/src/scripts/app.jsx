e7c418fac139f5ae6e290c834824254358999cfb
Dihtiar Artem <digtyar@river-soft.net>
2017-05-10 16:57:58 +0300

    added (app) simple search

--- client/src/scripts/app.jsx
+++ client/src/scripts/app.jsx
@@ -1,21 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './test.jsx';
+// import Test from './test.jsx';
 
-class App extends React.Component {
-    
+let test = [{
+    id: 1,
+    name: 'Yoda'
+},
+{
+    id: 2,
+    name: 'Obi Van'
+},{
+    id: 3,
+    name: 'C3PO'
+},
+{
+    id: 4,
+    name: 'Bill'
+}];
 
-    constructor() {
-        super();
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
     }
-    render () {
+
+    render() {
         return (
             <div>
-            Hello world
-            <Test />
+                <input type="text" value={this.props.value} onChange={(event)=> {
+                        this.props.onChange(event.target.value);
+                    }} />
             </div>
         );
     }
 }
 
-ReactDOM.render(App, document.getElementById('app'));
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: '',
+            items: test
+        };
+    }
+
+    renderList() {
+        let list = this.state.items.filter((item)=> {
+            return item.name.match(this.state.search);
+        })
+        return (
+            <ul>
+                {list.map((item)=> {
+                    return (
+                        <li key={item.id}>{item.name}</li>
+                    );
+                })}
+            </ul>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                Hello world
+                <Input value={this.state.search} onChange={(data)=>{
+                        this.setState({
+                            search: data
+                        });
+                }}/>
+                {this.state.search}
+                {this.renderList.call(this)}
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));

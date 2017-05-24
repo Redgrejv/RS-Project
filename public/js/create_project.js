4e1b9a07a22ee0dfebe96b6daef4fc6efd2fd3f5
Николай Дымура <kolyav1ampir@gmail.com>
2017-05-24 16:56:12 +0300

    add project and project name

--- public/js/create_project.js
+++ public/js/create_project.js
@@ -1,10 +1,21 @@
-$('.create_project').click(function(){
+$(document).ready(function() {
+	$('.create_project').click(function(){
 		var project_name = $('#project_name').val();
 		var his = $(this).closest('body').find('.new_project');
     	$('.projects').append('<div class="added_project">'+
             '<div class="project_name">'+
-                project_name +
+                '<p>'+project_name+'</p>' +
             '</div>'+
         '</div>');
     	his.hide();
-    }   ); 
+    }   );
+
+})
+// $('.added_project').click(function(){
+// 		var project_name = $('#project_name').val();
+// 		// var his = $(this).closest('body').find('.new_project');
+//     	$('.task_panel').append('<div class="project_name">'+
+//                 project_name +
+//             '</div>');
+//     	// his.hide();
+//     }   ); 

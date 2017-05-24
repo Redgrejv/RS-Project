61476555a2382c327bd3bd7b2e69a6d5f27764e5
Redgrejv <redgrejv10@gmail.com>
2017-05-25 01:20:58 +0300

    remove projects by projectID

--- routes/modules/administration_project.js
+++ routes/modules/administration_project.js
@@ -3,13 +3,20 @@ var HttpError   = require('../../error').HttpError;
 var Project     = require('../../models/projects').Project;
 // var mongoose    = require('mongoose');
 
+function getReqData(req){
+
+    return data = {
+        title: req.body.title,
+        id_user: req.tokenObj.id
+    };
+}
+
 exports.createProject = function (req, res, next) {
-    var title = req.body.title;
-    var id_user = req.tokenObj.id;
+    var data = getReqData(req);
 
     var new_project = new Project({
-        title: title,
-        _parent: [id_user]
+        title: data.title,
+        _parent: [data.id_user]
     });
 
     console.log(new_project);
@@ -21,25 +28,32 @@ exports.createProject = function (req, res, next) {
 };
 
 exports.updateProject = function (req, res, next) {
-    var title = req.body.title;
-    var new_title = req.body.new_title;
-    var id_user = req.tokenObj.id;
+    var data = getReqData(req);
 
-    Project.update({_parent: [id_user], title: title}, {title: new_title}, function (err) {
+    var new_title = req.body.new_title;
+
+    Project.update({_parent: [data.id_user], title: data.title}, {title: new_title}, function (err) {
         if(err) return next(err);
 
-        res.status(200).json({message: 'Данные обновлены'});
+        res.json({message: 'Данные обновлены'});
     });
 };
 
 exports.removeProject = function (req, res, next) {
+    var data = getReqData(req);
+    var _id_project = req.data.id;
 
+    Project.remove({_parent: [data.user_id], _id: _id_project}, function (err, result) {
+        if(err) return next(err);
+
+        res.json({message: 'Проект удален'});
+    })
 };
 
 exports.getUserAllProject = function (req, res, next) {
-    var user_id = req.tokenObj.id;
+    var data = getReqData(req);
 
-    Project.find({_parent: [user_id]}, function (err, projects) {
+    Project.find({_parent: [data.user_id]}, function (err, projects) {
         if(err) return next(err);
 
         res.json(projects);

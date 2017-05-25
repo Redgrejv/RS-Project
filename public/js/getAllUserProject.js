ab06b9db36d492ef70e861ff047df074538fd31e
Redgrejv <redgrejv10@gmail.com>
2017-05-25 22:51:04 +0300

    refactor

--- public/js/getAllUserProject.js
+++ public/js/getAllUserProject.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $.ajax({
         type: "POST",
-        url: "http://localhost:3000/api/project/getUserAllProject",
+        url: "http://localhost:3000/api/projects",
         headers: { 'Authentication': localStorage('token') },
         success: function(response) {
 

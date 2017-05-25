ebf42c844f475372dc6098e9ee7162ccd97b28a6
Redgrejv <redgrejv10@gmail.com>
2017-05-25 21:11:10 +0300

    temp

--- public/js/getAllUserProject.js
+++ public/js/getAllUserProject.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $.ajax({
         type: "POST",
-        url: "https://localhost:3000/api/project/getUserAllProject",
+        url: "http://localhost:3000/api/project/getUserAllProject",
         headers: { 'Authentication': localStorage('token') },
         success: function(response) {
 

8f24b07cfe91b07154217ebe80331d0a3983c395
Redgrejv <redgrejv10@gmail.com>
2017-05-27 17:04:43 +0300

    animation block add project

--- public/js/show_add_project.js
+++ public/js/show_add_project.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
     $(".show_add_project").click(function() {
-        var add = $(this).closest('body').find('.new_project');
-        if (!add.is(':visible')) {
-            $('.new_project').hide();
-            add.show();
+        // var add = $(this).closest('body').find('.new_project');
+        // if (!add.is(':visible')) {
+        //     $('.new_project').hide();
+        //     add.show();
+        // }
+
+        var div_newProject = $('.new_project');
+
+        if (div_newProject.hasClass('hide')) {
+            div_newProject.show('drop', 500, function() {
+                $(this).removeClass('hide');
+            });
+        } else {
+            div_newProject.hide('drop', 500, function() {
+                $(this).addClass('hide');
+            });
         }
+
+
+
+
         // else{add.hide();}
         // $(this).closest('body').find('.menu_list').hide();
     });

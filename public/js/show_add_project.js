875cdd28057f5802057da717c303578d458bbc6f
Redgrejv <redgrejv10@gmail.com>
2017-05-27 21:41:14 +0300

    fix

--- public/js/show_add_project.js
+++ public/js/show_add_project.js
@@ -9,19 +9,25 @@ $(document).ready(function() {
         var div_newProject = $('.new_project');
 
         if (div_newProject.hasClass('hide')) {
-            div_newProject.show('drop', 500, function() {
+            div_newProject.show('drop', 400, function() {
                 $(this).removeClass('hide');
             });
         } else {
-            div_newProject.hide('drop', 500, function() {
+            div_newProject.hide('drop', 400, function() {
                 $(this).addClass('hide');
             });
         }
-
-
-
-
         // else{add.hide();}
         // $(this).closest('body').find('.menu_list').hide();
+    });
+
+    $(document).mouseup(function(e) {
+        var div = $(".new_project");
+        if (!div.is(e.target) &&
+            div.has(e.target).length === 0) {
+            div.hide('drop', 400, function() {
+                $('.new_project').addClass('hide');
+            });
+        }
     });
 });

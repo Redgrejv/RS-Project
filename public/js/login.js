7f7037f5ed5125ff4706676bb19363a7edb7372e
Redgrejv <redgrejv10@gmail.com>
2017-05-29 00:16:43 +0300

    Revert "fix code"
    This reverts commit 419bd63ee486c6ba414f4d7200a7edcd66093d7d.

--- public/js/login.js
+++ public/js/login.js
@@ -1,112 +1,96 @@
-$(document).ready(function() {
-        var form = $('.rf');
-        var btn = $('#sign_in');
+$(document).ready(function () {
+    var form = $('.rf');
+    var btn=form.find('.login');
+    form.submit(function (event) {
+        event.preventDefault();
+    });
+    form.find('.field').addClass('empty');
 
-        buttonDisable();
+    function checked(){
 
-        form.find('.field').addClass('empty');
-
-        function checked() {
-
-            form.find('.field').each(function() {
-                if ($(this).val() != '') {
-                    $(this).removeClass('empty');
-                } else {
-                    $(this).addClass('empty');
+        form.find('.field').each(function(){
+            if($(this).val()!=''){
+                $(this).removeClass('empty');
                 }
-            });
-        }
-
-        function EmptyFilds() {
-            form.find('.empty').css({ 'border-bottom': '1px solid red' });
-
-            setTimeout(function() {
-                form.find('.empty').removeAttr('style');
-
-            }, 100);
-        }
-
-        $('#email').keyup(function() {
-            var pattern = /^([a-zA-Z0-9_\.-]{2,20})+@[a-z0-9-]+\.([a-z],{2,4}\.)?[a-z]{2,4}$/i;
-            if ($(this).val() != '') {
-                if (pattern.test($(this).val())) {
-                    $(this).removeClass('empty');
-                    $(this).addClass('ok');
-                    $('.lmail').text('');
-                } else {
-                    $(this).addClass('empty');
-                    $(this).removeClass('ok');
-                    $('.lmail').text('Почта введена некоректно');
-                }
-            } else {
+            else{
                 $(this).addClass('empty');
-                $(this).removeClass('ok');
-                $('.lmail').text('Введите почту');
             }
-
-            buttonDisable();
         });
-
-        $('#password').keyup(function() {
-            var regular = /^([a-zA-Z0-9]){2,20}$/;
-            if ($(this).val() != '') {
-                if (regular.test($(this).val())) {
-                    $(this).removeClass('empty');
-                    $(this).addClass('ok');
-                    $('.lpass').text('');
-                } else {
-                    $(this).addClass('empty');
-                    $(this).removeClass('ok');
-                    $('.lpass').text('Пароль введен некоректно');
-                }
-            } else {
-                $(this).addClass('empty');
-                $(this).removeClass('ok');
-                $('.lpass').text('Введите пароль');
-            }
-
-            buttonDisable();
-        });
-
-        setInterval(function() {
-            checked();
-
-        }, 100);
-
-        form.submit(function(event) {
-            event.preventDefault();
-        });
-
-        $('#sign_in').click(function(evet) {
-            event.preventDefault();
-
-            $.ajax({
-                url: 'http://localhost:3000/api/autorize',
-                type: 'POST',
-                data: {
-                    email: $('#email').val(),
-                    password: $('#password').val()
-                },
-                success: function(data) {
-                    localStorage.setItem('token', data.token);
-                    windows.location.href = 'index.html';
-                },
-                error: function(data) {
-                    $('.lpass').text('Oshibka');
-                }
-
-            })
-        });
-    })
-    //А-ЯёЁ регулярка кирилица
-
-function buttonDisable() {
-    sizeOk = $('.rf').find('.ok').length;
-    if (sizeOk > 1) {
-        $('#sign_in').prop('disabled', false);
-        $('#sign_in').removeClass('disable');
-    } else {
-        $('#sign_in').prop('disabled', true);
-        $('#sign_in').addClass('disable');
     }
+
+function EmptyFilds(){
+    form.find('.empty').css({'border-bottom':'1px solid red'});
+
+    setTimeout(function(){
+        form.find('.empty').removeAttr('style');
+
+    },500);
 }
+
+    form.find('#email').blur(function(){
+            var pattern=/^([a-zA-Z0-9_\.-]{2,20})+@[a-z0-9-]+\.([a-z],{2,4}\.)?[a-z]{2,4}$/i;
+            if($(this).val() != '') {
+            if(pattern.test($(this).val())){
+                $(this).removeClass('empty');
+                $(this).addClass('ok');
+                $('.lmail').text('');
+            }else{
+            $('.lmail').text('Почта введена некоректно');
+        }
+}
+else {
+    $('.lmail').text('Введите почту');
+}
+});
+
+    form.find('#password').blur(function(){
+            var regular=/^([a-zA-Z0-9]){2,20}$/;
+            if($(this).val() != '') {
+            if(regular.test($(this).val())){
+                $(this).removeClass('empty');
+                $(this).addClass('ok');
+                $('.lpass').text('');
+            }else{
+            $('.lpass').text('Пароль введен некоректно');
+        }
+    }
+    else{ 
+        $('.lpass').text('Введите пароль');
+    }
+    });
+
+setInterval(function(){
+    checked();
+    sizeOk=form.find('.ok').length;
+    if(sizeOk>1){
+        btn.attr('disabled','false');
+        btn.removeClass('disable');
+    }
+    else{
+        btn.attr('disabled','true');
+        btn.addClass('disable');
+    }
+},500);
+
+form.submit(function(event){
+	event.parentDefault();
+});
+
+btn.click(function(evet){
+	$.ajax({
+		url: 'http://localhost:3000/api/autorize',
+		type: 'POST',
+		data: {email: $('#email').val(),
+			   password: $('#password').val()},
+		success: function(data){
+				localStorage.setItem('token', data.token);
+				windows.location.href='index.html';
+		},
+		error: function(data){
+			 $('.lpass').text('Oshibka');		
+		}
+
+		})
+});
+})
+//А-ЯёЁ регулярка кирилица

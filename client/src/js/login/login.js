2d6536666269d27c43f1a95745331e149dd3398d
Valentina <Anirva1994@gmail.com>
2017-06-08 16:29:18 +0300

    keyup -> blur

--- client/src/js/login/login.js
+++ client/src/js/login/login.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
             });
         }
 
-        form.find('#login-form__email').on('keyup',function() {
+        form.find('#login-form__email').on('blur',function() {
             var pattern = /^([a-zA-Z0-9_\.-]{2,20})+@[a-z0-9-]+\.([a-z],{2,4}\.)?[a-z]{2,4}$/i;
             if ($(this).val() != '') {
                 if (pattern.test($(this).val())) {
@@ -50,7 +50,7 @@ $(document).ready(function() {
                     checked();
         });
 
-        form.find('#login-form__password').on('keyup',function() {
+        form.find('#login-form__password').on('blur',function() {
             var regular = /^([a-zA-Z0-9]){2,20}$/;
             if ($(this).val() != '') {
                 if (regular.test($(this).val())) {
@@ -73,7 +73,7 @@ $(document).ready(function() {
         $('#login-form__button-login').click(function(evet) {
             event.preventDefault();
             $.ajax({
-                url: 'http://localhost:3000/api/autorize',
+                url: 'http://localhost:3000/api/users/login',
                 type: 'POST',
                 data: {
                     email: $('#login-form__email').val(),

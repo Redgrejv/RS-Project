6de95479f1ea032fb1156f00eebc388c0a642280
beznoschenko <Anirva1994@gmail.com>
2017-06-26 16:48:37 +0300

    Fix endex page

--- client/src/js/index/editProject.js
+++ client/src/js/index/editProject.js
@@ -1,19 +1,31 @@
-  $(document).ready(function() {
-      $('.menu__projects').on('click', '.icon-edit', function(){
+$(document).ready(function () {
+    //показ модального окна редактирования проекта
+    $('.menu__projects').on('click', '.icon-edit', function () {
         event.preventDefault();
-        var prname = prompt("Enter new project name", "Project");
+        $('#overlay').fadeIn(400, function () {
+            $('.modal-window-edit').css({ 'display': 'block', 'opacity': '1', 'top': '35%' });
+        });
+        $('#edit-project-form__name').val('');
+    });
+
+
+    $('#edit-project-form__button').on('click', function (event) {
+        event.preventDefault();
+        var prname = $('#edit-project-form__name').val();
         $.ajax({
             type: "PATCH",
             url: "http://localhost:3000/api/projects/" + localStorage.getItem('project.id'),
-            headers:{ 'Authorization': localStorage.getItem('token') },
+            headers: { 'Authorization': localStorage.getItem('token') },
             data: { newTitle: prname },
-            success: function(res){
-                $('#'+localStorage.getItem('project.id')).find('.menu__project-name').text(prname);
-				$('.content__project-name').text(prname);
+            success: function (res) {
+                $('#' + localStorage.getItem('project.id')).find('.menu__project-name').text(prname);
+                $('.content__project-name').text(prname);
+                $('.icon-close').click();
+                $('#' + localStorage.getItem('project.id')).find('.icon-back').click();
             },
-            error: function(res){
+            error: function (res) {
 
             }
         });
-  })
-  })
+    })
+})

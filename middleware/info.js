22210f4edfb13c8f939679326278a0a48644ce15
Redgrejv <redgrejv10@gmail.com>
2017-07-12 15:45:20 +0300

    refactor

--- middleware/info.js
+++ middleware/info.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created by redgr on 25.04.2017.
  */
@@ -15,15 +14,4 @@ function getVersion() {
     return new Promise(function (resolve, reject) {
         return resolve(module.exports.version);
     })
-=======
-/**
- * Created by redgr on 25.04.2017.
- */
-
-
-var pkginfo = require('pkginfo')(module);
-
-module.exports.getVersion = function(req, res, next) {
-    res.json(module.exports.version);
->>>>>>> origin/34-
 };
